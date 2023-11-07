a04102de3ac452640fc34ad59c1fbf36fe2ac258
Andrea Spreafico <andrea.spreafico@bvnk.com>
2023-11-07 23:26:37 +0100

    Managed to add icons in a view and also in the navigation bar

--- index.js
+++ index.js
@@ -1,175 +1,186 @@
 import React from 'react';
-import { Image, View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { Navigation } from 'react-native-navigation';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+// Define the Home Icon component
 const HomeIcon = () => (
-  <Icon name="home" size={30} color="#900" />
-  );
-  
-  const ProfileIcon = () => (
-    <Image
-      source={require('./src/assets/user.png')}
-      style={{ width: 24, height: 24 }}
-      resizeMode="contain"
+  <MaterialIcons name="home" size={30} color="#900" />
+);
+
+// Login Screen component
+const LoginScreen = () => (
+  <View style={styles.root}>
+    <Text> Here it is going to be implemented the login form </Text>
+    <Button
+      title='Login'
+      color='#710ce3'
+      onPress={() => Navigation.setRoot(mainRoot)}
     />
-  );
-  
-const LoginScreen = () => {
-    return (
-      <View style={styles.root}>
-        <Text> Here it is going to be implemented the login form </Text>
-        <Button
-          title='Login'
-          color='#710ce3'
-          onPress={() => Navigation.setRoot(mainRoot)}
-        />
-      </View>
-    );
-  };
-  
-  const HomeScreen = (props) => {
-    return (
-      <View style={styles.root}>
-        <HomeIcon/>
-        <Text>Hello React Native Navigation 👋</Text>
-        <Button
-          title='Push Settings Screen'
-          color='#710ce3'
-          onPress={() => Navigation.push(props.componentId, {
-            component: {
-              name: 'Settings',
-              passProps: {
-                name: 'TestPropsToPass',
-                status: 'TestPropsToPass'
-              }
-            }
-          })} />
-      </View>
-    );
-  };
-  HomeScreen.options = {
-    topBar: {
-      title: {
-        text: 'Home'
-      }
-    },
-    bottomTab: {
+  </View>
+);
+
+// Home Screen component
+const HomeScreen = (props) => (
+  <View style={styles.root}>
+    <HomeIcon/>
+    <Text>Hello React Native Navigation 👋</Text>
+    <Button
+      title='Push Settings Screen'
+      color='#710ce3'
+      onPress={() => Navigation.push(props.componentId, {
+        component: {
+          name: 'Settings',
+          passProps: {
+            name: 'TestPropsToPass',
+            status: 'TestPropsToPass'
+          }
+        }
+      })} />
+  </View>
+);
+
+// Settings Screen component
+const SettingsScreen = () => (
+  <View style={styles.root}>
+    <Text>Settings Screen</Text>
+  </View>
+);
+
+// Component options
+HomeScreen.options = {
+  topBar: {
+    title: {
       text: 'Home'
     }
-  };
-  
-  const SettingsScreen = () => {
-    return (
-      <View style={styles.root}>
-        <Text>Settings Screen</Text>
-      </View>
-    );
+  },
+  bottomTab: {
+    text: 'Home'
   }
-  SettingsScreen.options = {
-    topBar: {
-      title: {
-        text: 'Settings'
-      }
-    },
-    bottomTab: {
+};
+
+SettingsScreen.options = {
+  topBar: {
+    title: {
       text: 'Settings'
     }
+  },
+  bottomTab: {
+    text: 'Settings'
   }
-  
-  Navigation.registerComponent('Login', () => LoginScreen);
-  Navigation.registerComponent('Home', () => HomeScreen);
-  Navigation.registerComponent('Settings', () => SettingsScreen);
-  
+};
+
+// Register the components
+Navigation.registerComponent('Login', () => LoginScreen);
+Navigation.registerComponent('Home', () => HomeScreen);
+Navigation.registerComponent('Settings', () => SettingsScreen);
+
+// Set default options
+Navigation.setDefaultOptions({
+  statusBar: {
+    backgroundColor: '#4d089a'
+  },
+  topBar: {
+    title: {
+      color: 'white'
+    },
+    backButton: {
+      color: 'white'
+    },
+    background: {
+      color: '#4d089a'
+    }
+  },
+  bottomTab: {
+    fontSize: 14,
+    selectedFontSize: 14
+  }
+});
+
+// App launched listener
+Navigation.events().registerAppLaunchedListener(async () => {
+  // Load the icons
+  const personIcon = await MaterialIcons.getImageSource('home', 25);
+  const settingsIcon = await MaterialIcons.getImageSource('settings', 25);
+
+  // Define the root based on the user's login status
   const mainRoot = {
     root: {
-        bottomTabs: {
-            id: 'BOTTOM_TABS_LAYOUT',
-            children: [
-              {
-                stack: {
-                  id: 'HOME_TAB',
-                  children: [
-                    {
-                      component: {
-                        id: 'HOME_SCREEN',
-                        name: 'Home'
-                      }
-                    }
-                  ],
-                  
-                  options: {
-                    bottomTab: {
-                        icon: HomeIcon
-                    }
-                    }
-                  }
-              },
-              {
-                stack: {
-                  id: 'PROFILE_TAB',
-                  children: [
-                    {
-                      component: {
-                        id: 'PROFILE_SCREEN',
-                        name: 'Settings'
-                      }
-                    }
-                  ],
-                  options: {
-                    bottomTab: {
-                        icon: ProfileIcon
-                    }
+      bottomTabs: {
+        id: 'BOTTOM_TABS_LAYOUT',
+        children: [
+          {
+            stack: {
+              id: 'HOME_TAB',
+              children: [
+                {
+                  component: {
+                    id: 'HOME_SCREEN',
+                    name: 'Home'
                   }
                 }
+              ],
+              options: {
+                bottomTab: {
+                  icon: personIcon,
+                  text: 'Home'
+                }
               }
-            ]
+            }
+          },
+          {
+            stack: {
+              id: 'PROFILE_TAB',
+              children: [
+                {
+                  component: {
+                    id: 'PROFILE_SCREEN',
+                    name: 'Settings'
+                  }
+                }
+              ],
+              options: {
+                bottomTab: {
+                  icon: settingsIcon,
+                  text: 'Settings'
+                }
+              }
+            }
           }
-    }
-  };
-  const loginRoot = {
-    root: {
-      component: {
-        name: 'Login'
+        ]
       }
     }
   };
-  
-  
-  Navigation.setDefaultOptions({
-    statusBar: {
-      backgroundColor: '#4d089a'
-    },
-    topBar: {
-      title: {
-        color: 'white'
-      },
-      backButton: {
-        color: 'white'
-      },
-      background: {
-        color: '#4d089a'
-      }
-    },
-    bottomTab: {
-      fontSize: 14,
-      selectedFontSize: 14
-    }
-  });
 
-  Navigation.events().registerAppLaunchedListener(async () => {
-    Navigation.setRoot(isLoggedIn() ? mainRoot : loginRoot);
-  });
-  
-  function isLoggedIn() {
-    return true
+  // Check if the user is logged in and set the root accordingly
+  if (isLoggedIn()) {
+    Navigation.setRoot(mainRoot);
+  } else {
+    Navigation.setRoot(loginRoot);
   }
-  
-  const styles = StyleSheet.create({
-    root: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'whitesmoke'
+});
+
+// Check if user is logged in (placeholder function)
+function isLoggedIn() {
+  // Replace with your actual login logic
+  return true;
+}
+
+// Styles for the components
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke'
+  }
+});
+
+// Define the login root (used if the user is not logged in)
+const loginRoot = {
+  root: {
+    component: {
+      name: 'Login'
     }
-  });
+  }
+};
